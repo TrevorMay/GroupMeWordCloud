099e0fac74c75c988ecbecc83f92471258f8d532
Trevor May <trevormay2017@gmail.com>
2021-05-24 10:32:57 -0600

    Finally came back and got this to page back through all the messages in a conversation not just the first 100, also trying committing and pushing from inside pycharm instead of Git Bash

--- main/main.py
+++ main/main.py
@@ -8,9 +8,12 @@ params = {'token': config.access_token, 'limit': 100, 'before_id': '160705055250
 
 r = requests.get(url, params=params)
 
-json_data = r.json()
-
-conversationText = [str(item['text']) for item in json_data['response']['messages']]
+while r.status_code != 304:
+    json_data = r.json()
+    conversationText = [str(item['text']) for item in json_data['response']['messages']]
+    max_index = len(json_data['response']['messages'])
+    params['before_id'] = json_data['response']['messages'][max_index-1]['id']
+    r = requests.get(url, params=params)
 
 wordCloud = WordCloud().generate(str(conversationText))
 
